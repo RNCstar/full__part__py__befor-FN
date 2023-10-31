fef6d99045dcc2719277d552678488bbfbaf780d
RNC <147319873+RNCstar@users.noreply.github.com>
2023-10-30 21:39:46 -0700

    add__list__point

---
+++ list.py
@@ -0,0 +1,20 @@
+# the list is a data type that can be used to like arrays
+# 1)Ordered 2)changeable 3) [1,2,3[5,7,8,]]
+# this data can follow all roles by string indexing
+
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+arr.insert(10, 11)  # first selected index then write a value
+print(arr)
+print("--------------------------------")
+# ----------------------------------------------------------------
+
+arr.append(12)  # first selected indexprint(arr)
+print(arr)
+print("--------------------------------")
+# ----------------------------------------------------------------
+students = ["ali", "reza", "gasem", "moied"]
+
+for x in students:
+    print("my student's name is =  \t"+x)
+    
