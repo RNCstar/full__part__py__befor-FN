51ed1fb6d139e7e2d5de76272e8ce385a3ec235a
RNC <147319873+RNCstar@users.noreply.github.com>
2023-11-03 19:41:11 -0700

    add__tuples--sets

---
+++ tupple__sets__dictionary.py
@@ -0,0 +1,19 @@
+# tuples
+# ordered ----  unchangeable ---- you can use more than one var
+numbers=(1,2,3,4,5,6,7,8,9)
+for x in numbers:
+    print(x)
+print("--------------------------------")
+    # ----------------------------------------------------------------
+
+    
+    
+    
+    
+# sets
+# unchangeable ---- you can remove var then add it
+alphabets={"a","b","c","d","e","f","g","h"}
+for x in alphabets:
+    print(x)
+print("--------------------------------")
+    # ----------------------------------------------------------------
