eedc5ec6d12598084a2e28b62287717f6182890d
RNC <147319873+RNCstar@users.noreply.github.com>
2023-11-09 12:35:55 -0800

    Add__If

---
+++ if.py
@@ -0,0 +1,17 @@
+# conditional ====> if 
+x=20
+if x== 20:
+    print("x is twenty")
+
+
+
+
+
+# 2) short if 
+if x>10 :print("x is bigger than 10")
+
+
+# 3) noT  OR   AND
+
+if x>5 or x >10 and x <25 :
+    print("x is OK")
